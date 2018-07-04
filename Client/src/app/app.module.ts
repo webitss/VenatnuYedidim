b786b0663810d15abf620bc6322996d94d1f2d44
rivkik <40832274+rivkik@users.noreply.github.com>
2018-07-04 11:55:00 -0700

    settings component

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -1,35 +1,33 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { BrowserModule} from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { HttpModule, Http } from "@angular/http";
 import { RouterModule, Route } from '@angular/router';
+
 //--- services ---
 import { AppProxy } from './services/app.proxy';
 //--- components ---
 import { AppComponent } from './components/app/app.component';
+import { SettingComponent } from './components/setting/setting.component';
+
 
 @NgModule({
   declarations: [
     //components
-    AppComponent    
+    AppComponent,
+    SettingComponent    
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
     HttpClientModule,   
-    // RouterModule.forRoot([
-    //   {
-    //     path: ":uGameId", component: ?,
-    //     children: [
-    //       { path: "", component: ? },
-    //       { path: "opening", component: ? },
-    //       { path: "clickers/:uDivisionId/:step", component: ? },
-    //       { path: "summary/:mode", component: ? },
-    //     ]
-    //   },
-    // ], { useHash: true }) 
+    RouterModule.forRoot([
+      {
+        path: "setting", component: SettingComponent
+      },
+    ], { useHash: true }) 
   ],
   providers: [AppProxy],
   bootstrap: [AppComponent]
