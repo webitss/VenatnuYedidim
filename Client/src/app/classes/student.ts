65a22b0c71e6f78f2f5a85e93d39efdfd6aa7e3f
shoshimazia <40861363+shoshimazia@users.noreply.github.com>
2018-07-05 17:41:19 +0300

    יצירת מחלקות person,student

---
+++ Client/src/app/classes/student.ts
@@ -0,0 +1,23 @@
+import { Person } from "./person";
+
+export class Student implements Person {
+    iPersonId: number;
+    nvFirstName: String;
+    nvLastName: String;
+    nvIdentityCard: String;
+    nvPhone: String;
+    nvBirthday:String;
+    dtBirthday:Date;
+    nvAddress: String;
+    nvCity: String;
+    iCreatedByUserId: number;
+    dtCreatedate: Date;
+    iLastModifyUserId: number;
+    dtLastModifyDate: Date;
+    bSysRowStatus: boolean;
+    nvStatus: String;
+    nvMobile: String;
+    nvEmail: String;
+
+    
+}
