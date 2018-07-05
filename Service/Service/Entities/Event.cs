921e7c26d99ab7ad703ddabddd88077918be0285
leaZon <40159502+leaZon@users.noreply.github.com>
2018-07-05 18:59:03 +0300

    functions

---
+++ Service/Service/Entities/Event.cs
@@ -0,0 +1,43 @@
+﻿using Service.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace Service.Entities
+{
+    [DataContract]
+    public class Event
+    {
+        [DataMember]
+        public int iEventId { get; set; }
+        [DataMember]
+        public string nvName { get; set; }
+        [DataMember]
+        public DateTime dDate { get; set; }
+        [DataMember]
+        public string nvPlace { get; set; }
+        [DataMember]
+        public string nvComments { get; set; }
+
+        public static List<Event> GetList(string nvUserName, string nvPassword)
+        {
+            try
+            {
+
+                DataRow dr = SqlDataAccess.ExecuteDatasetSP("TEvent_SLCT").Tables[0].Rows[0];
+                //User user = ObjectGenerator<User>.GeneratFromDataRow(dr);
+                List<Event> events=ObjectGenerator<List<Event>>.GeneratFromDataRow(dr);
+                return events;
+            }
+            catch (Exception ex)
+            {                
+                Log.LogError("GetList / TEvent_SLCT","ex"+ex);
+                return null;
+            }
+        }
+    }
+}
---
+++ Service/Service/Entities/Participant.cs
@@ -0,0 +1,24 @@
+﻿using Service.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace Service.Entities
+{
+    [DataContract]
+    public class Participant
+    {
+    
+        [DataMember]
+        public int iEventId { get; set; }
+        [DataMember]
+        public int iPersonId { get; set; }
+        [DataMember]
+        public int iArrivalStatus { get; set; }
+
+    }
+}
