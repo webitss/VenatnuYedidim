3dee2ad073eea338f0cdb440c08f08096dcf5243
leaZon <40159502+leaZon@users.noreply.github.com>
2018-07-12 19:13:37 +0300

    update appModule

---
+++ Client/src/app/student-meeting-details/student-meeting-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-student-meeting-details',
+  templateUrl: './student-meeting-details.component.html',
+  styleUrls: ['./student-meeting-details.component.css']
+})
+export class StudentMeetingDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
