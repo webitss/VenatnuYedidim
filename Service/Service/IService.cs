26061cf01d0dcb4654f8069deab0c973d9e1c53f
rikibenzaken <40832162+rikibenzaken@users.noreply.github.com>
2018-07-17 18:05:41 +0300

    lkhk

--- Service/Service/IService.cs
+++ Service/Service/IService.cs
@@ -148,14 +148,6 @@ namespace Service
         List<Student> GetAvrechStudents(int iPersonId);
 
 
-        [WebInvoke(
-           Method = "POST",
-           UriTemplate = "GetConversations",
-           BodyStyle = WebMessageBodyStyle.WrappedRequest,
-           ResponseFormat = WebMessageFormat.Json,
-           RequestFormat = WebMessageFormat.Json)]
-        List<Conversation> GetConversations(int iPersonId);
-
 
 
 
