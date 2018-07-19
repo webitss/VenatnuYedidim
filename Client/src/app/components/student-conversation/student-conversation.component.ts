925af58d383f2852dc0ed994b13cd7461a2c7c55
elishmiri <40832417+elishmiri@users.noreply.github.com>
2018-07-19 06:29:34 -0700

    con

--- Client/src/app/components/student-conversation/student-conversation.component.ts
+++ Client/src/app/components/student-conversation/student-conversation.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AppProxy } from '../../services/app.proxy';
+import { Conversation } from '../../classes/conversation';
 
 @Component({
   selector: 'app-student-conversation',
@@ -6,8 +8,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./student-conversation.component.css']
 })
 export class StudentConversationComponent implements OnInit {
+conversation:Conversation;
+  constructor(private appProxy:AppProxy) { }
 
-  constructor() { }
+  deleteConversation(iConversationId:number,iUserId:number)
+  {
+    this.appProxy.post("DeleteConversations",{iConversationId:iConversationId,iUserId:iUserId})
+    .then(
+      data=>{
+        this.conversation=data;
+        alert("sucsses");
+      }).catch(err=>{
+      alert(err);
+      });
+  }  
+
 
   ngOnInit() {
   }
