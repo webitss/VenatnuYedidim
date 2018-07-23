99cf875e8a0be3efb7358040964ceab3a34ff351
rivkik <40832274+rivkik@users.noreply.github.com>
2018-07-23 08:34:58 -0700

    update

--- Client/src/app/components/event-participants/event-participants.component.ts
+++ Client/src/app/components/event-participants/event-participants.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AppProxy } from '../../services/app.proxy';
 import { Participants } from '../../classes/participants';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-event-participants',
@@ -8,21 +9,47 @@ import { Participants } from '../../classes/participants';
   styleUrls: ['./event-participants.component.css']
 })
 export class EventParticipantsComponent implements OnInit {
-  protected iEventId: number = 1;
+  protected iEventId: number;
   protected participant: Array<any> = new Array<any>();
   protected lstColumns = [{
-      title: 'שם פרטי',
-      name: 'nvFirstName'    
-    },{
       title: 'שם משפחה',
       name: 'nvLastName'    
-    }
+    },{
+      title: 'שם פרטי',
+      name: 'nvFirstName'    
+    },
+    {
+      title: 'טלפון',
+      name: 'nvPhone'    
+    },
+    {
+      title: 'נייד',
+      name: 'nvMobile'    
+    },
+    {
+      title: 'מייל',
+      name: 'nvEmail'    
+    },
+    {
+      title: 'סוג משתתף',
+      name: 'iArrivalStatusType'    
+    },
+    {
+      title: 'סטטוס הגעה',
+      name: 'nvValue'    
+    },
+    
+    
+
+
 
   ]
 
-  constructor(private appProxy: AppProxy) { }
+  constructor(private appProxy: AppProxy, private route:ActivatedRoute) { }
 
   ngOnInit() {
+this.iEventId =parseInt( this.route.snapshot.paramMap.get('IEventId'));
+    
     this.appProxy.post("GetParticipantsList", { iEventId: this.iEventId })
       .then(
         data => {
