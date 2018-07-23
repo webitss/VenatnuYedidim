db5c8cb399e0c1b8bb2e21aea29f957d45746f15
rivkik <40832274+rivkik@users.noreply.github.com>
2018-07-23 08:46:53 -0700

    Revert "update"
    This reverts commit 99cf875e8a0be3efb7358040964ceab3a34ff351.

--- Client/src/app/components/event-participants/event-participants.component.ts
+++ Client/src/app/components/event-participants/event-participants.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AppProxy } from '../../services/app.proxy';
 import { Participants } from '../../classes/participants';
-import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-event-participants',
@@ -9,47 +8,21 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./event-participants.component.css']
 })
 export class EventParticipantsComponent implements OnInit {
-  protected iEventId: number;
+  protected iEventId: number = 1;
   protected participant: Array<any> = new Array<any>();
   protected lstColumns = [{
-      title: 'שם משפחה',
-      name: 'nvLastName'    
-    },{
       title: 'שם פרטי',
       name: 'nvFirstName'    
-    },
-    {
-      title: 'טלפון',
-      name: 'nvPhone'    
-    },
-    {
-      title: 'נייד',
-      name: 'nvMobile'    
-    },
-    {
-      title: 'מייל',
-      name: 'nvEmail'    
-    },
-    {
-      title: 'סוג משתתף',
-      name: 'iArrivalStatusType'    
-    },
-    {
-      title: 'סטטוס הגעה',
-      name: 'nvValue'    
-    },
-    
-    
-
-
+    },{
+      title: 'שם משפחה',
+      name: 'nvLastName'    
+    }
 
   ]
 
-  constructor(private appProxy: AppProxy, private route:ActivatedRoute) { }
+  constructor(private appProxy: AppProxy) { }
 
   ngOnInit() {
-this.iEventId =parseInt( this.route.snapshot.paramMap.get('IEventId'));
-    
     this.appProxy.post("GetParticipantsList", { iEventId: this.iEventId })
       .then(
         data => {
