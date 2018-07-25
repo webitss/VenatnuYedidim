847238b95663a1f55245285d583bcdc79e43ec94
naomi111 <40832045+naomi111@users.noreply.github.com>
2018-07-25 10:41:40 -0700

    by!!!

--- Client/src/app/components/event-details/event-details.component.ts
+++ Client/src/app/components/event-details/event-details.component.ts
@@ -13,8 +13,6 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class EventDetailsComponent implements OnInit {
 
-  @Input()
-  @Output()
   protected e: Event1;
 
   
@@ -23,15 +21,14 @@ export class EventDetailsComponent implements OnInit {
 // getHeader(){
 //   return this.header;
 // }
-  protected date: Date;
 
   
 // getUser(){
   
 // }
   save() {
-   this.date=new Date(this.e.dtEventDate);
-   this.e.dtEventDate = new Date(this.date);
+ //  this.date=new Date(this.e.dtEventDate);
+   this.e.dtEventDate = new Date(this.e.dtEventDate);
 
     this.appProxy.post('SetEvent', { oEvent: this.e,  iUserId: 1})
       .then(
@@ -49,13 +46,11 @@ export class EventDetailsComponent implements OnInit {
   constructor(private appProxy: AppProxy, private router: ActivatedRoute) { }
 
   ngOnInit() {
-    this.date=new Date();
     this.router.params.subscribe(params => {
       if (params['iEventId'] != '0') {
         this.appProxy.post("GetEvent", { iEventId: params['iEventId'] })
           .then(data => {
             this.e = data;
-            this.date=this.e.dtEventDate;
           //this.header=this.e.nvName;
   
           });
