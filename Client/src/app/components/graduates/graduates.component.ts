09f74416b083259f276ed7f76f619dd1766213ee
elishmiri <40832417+elishmiri@users.noreply.github.com>
2018-08-15 06:57:47 -0700

    update graduates

--- Client/src/app/components/graduates/graduates.component.ts
+++ Client/src/app/components/graduates/graduates.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { VyTableColumn } from '../../templates/vy-table/vy-table.classes';
+import { AppProxy } from '../../services/app.proxy';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Student } from '../../classes/student';
 
 @Component({
   selector: 'app-graduates',
@@ -7,9 +11,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GraduatesComponent implements OnInit {
 
-  constructor() { }
+  
+  constructor(private appProxy: AppProxy, private router: Router,private route: ActivatedRoute ) { }
+  param:any;
+  id: number;
+  studentList: Student[];
 
+  public lstColumns: Array<VyTableColumn> = new Array<VyTableColumn>();
   ngOnInit() {
-  }
 
+    
+  
+    this.id = 0;
+
+    this.appProxy.post('GetGraduatesList', { iUserId: this.id }).then(data => {
+      this.studentList = data;
+
+      this.studentList.forEach(
+        st => {
+           st['edit'] = '<div class="edit"></div>'; 
+          })
+    }, err => { alert(err); });
+
+
+    this.lstColumns.push(new VyTableColumn('עריכה', 'edit', 'html', true));
+    this.lstColumns.push(new VyTableColumn('שם פרטי', 'nvFirstName'));
+    this.lstColumns.push(new VyTableColumn('שם משפחה', 'nvLastName'));
+    this.lstColumns.push(new VyTableColumn('טלפון', 'nvPhone'));
+    this.lstColumns.push(new VyTableColumn('נייד', 'nvMobile'));
+    this.lstColumns.push(new VyTableColumn('דו"אל', 'nvEmail'));
+    this.lstColumns.push(new VyTableColumn('מוסד לימודים', 'nvYeshivaName'));
+
+}
 }
