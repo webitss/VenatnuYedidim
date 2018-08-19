a1fe9a5e6ac0cf9b5d63b0bf744f826a7a0eb1ae
leadwebit <lea.d@webit-sys.com>
2018-08-19 16:54:21 +0300

    aaa

--- Client/src/app/components/student-conversation-details/student-conversation-details.component.ts
+++ Client/src/app/components/student-conversation-details/student-conversation-details.component.ts
@@ -2,11 +2,8 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { AppProxy } from '../../services/app.proxy';
 import { Conversation } from '../../classes/conversation';
-<<<<<<< HEAD
 import { SysTableRow } from '../../classes/SysTableRow';
-=======
 import { Task } from '../../classes/task';
->>>>>>> 2cb1f8fed6c820dd05e57d5a241e70852f7b5ca6
 
 @Component({
   selector: 'app-student-conversation-details',
