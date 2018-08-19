e27b35eecae0aeabd41a50b1624b6689376be49e
leadwebit <lea.d@webit-sys.com>
2018-08-19 19:59:41 +0300

    סוף יום

--- Client/src/app/classes/task.ts
+++ Client/src/app/classes/task.ts
@@ -5,16 +5,18 @@ export class Task {
     public iTaskId: number;
     public iTaskType: number;
     public dtTaskdate: Date;
-    public TaskTime:Time;
-    public iCreatedByUserId: number;
-    public dtCreatedate: Date;
-    public iLastModifyUserId: number;
-    public dtLastModifyDate: Date;
-    public bSysRowStatus: boolean;
+    public tTaskTime:Time;
+    // public iCreatedByUserId: number;
+    // public dtCreatedate: Date;
+    // public iLastModifyUserId: number;
+    // public dtLastModifyDate: Date;
+    // public bSysRowStatus: boolean;
     
     public constructor() 
     {
-        this.dtTaskdate = new Date();
+        this.iTaskId=0;
+        // this.iTaskType=0;
+       // this.dtTaskdate = new Date();
     }
 
 }
