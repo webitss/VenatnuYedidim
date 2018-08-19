55bae1bacda6da9fcbfb5f424d5fb8a17a5786cf
sari <sari0533155016@gmail.com>
2018-08-19 19:15:02 +0300

    המרה לאקסל

--- Client/src/app/templates/vy-table/vy-table.component.ts
+++ Client/src/app/templates/vy-table/vy-table.component.ts
@@ -91,46 +91,28 @@ export class VyTableComponent implements OnInit {
   }
 
 
-
-
-
+  // var tableToExcel = (function() {
+  //   var uri = 'data:application/vnd.ms-excel;base64,'
+  //     , template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body><table>{table}</table></body></html>'
+  //     , base64 = function(s) { return window.btoa(unescape(encodeURIComponent(s))) }
+  //     , format = function(s, c) { return s.replace(/{(\w+)}/g, function(m, p) { return c[p]; }) }
+  //   return function(table, name) {
+  //     if (!table.nodeType) table = document.getElementById(table)
+  //     var ctx = {worksheet: name || 'Worksheet', table: table.innerHTML}
+  //     window.location.href = uri + base64(format(template, ctx))
+  //   }
+  // })()
+  
 
   public tableToExcel() {
-    debugger;
     let uri = 'data:application/vnd.ms-excel;base64,'
-      , template = `<html xmlns:o="urn:schemas-microsoft-com:office:office" 
-          xmlns:x="urn:schemas-microsoft-com:office:excel" 
-          xmlns="http://www.w3.org/TR/REC-html40">
-          <head>
-            <meta http-equiv="content-type" content="text/plain; charset=utf-8"/>
-            <!--[if gte mso 9]><xml>
-            <x:ExcelWorkbook>
-            <x:ExcelWorksheets>
-            <x:ExcelWorksheet>
-            <x:Name>{worksheet}</x:Name>
-            <x:WorksheetOptions>
-            <x:DisplayGridlines/>
-            </x:WorksheetOptions>
-            </x:ExcelWorksheet>
-            </x:ExcelWorksheets>
-            </x:ExcelWorkbook>
-            </xml>
-            <![endif]-->            
-          </head>
-          <body>
-            <table>{table}</table>
-          </body>
-          </html>`
-      , base64 = function (s) {
-        return window.btoa(decodeURIComponent(encodeURIComponent(s)))
-      }
+    , template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body><table>{table}</table></body></html>'
+    , base64 = function(s) { return window.btoa(eval('unescape(encodeURIComponent(s))')) }
       , format = function (s, c) {
         return s.replace(/{(\w+)}/g, function (m, p) { return c[p]; })
       }
-
-    var ctx = { worksheet: name || 'Worksheet', table: this.createTableFromData() }
+    var ctx = { worksheet: name || 'Worksheet', table: this.createTableFromData()}
     window.location.href = uri + base64(format(template, ctx))
   }
-
 
 }
