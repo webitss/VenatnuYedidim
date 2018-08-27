9da77cfd726c0f4e90e22b4972fb571cae10a07a
dvoraack <dvoaack@gamil.com>
2018-08-27 20:03:29 +0300

    סוף יום

--- Client/src/app/components/task/task.component.ts
+++ Client/src/app/components/task/task.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, Input,Output, EventEmitter } from '@angular/core';
-import{Task} from "../../classes/task"
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Task } from "../../classes/task"
 import { SysTableService } from '../../services/sys-table.service';
 import { AppProxy } from '../../services/app.proxy';
 import { GlobalService } from '../../services/global.service';
@@ -11,19 +11,19 @@ import { GlobalService } from '../../services/global.service';
 })
 export class TaskComponent implements OnInit {
 
-@Input()
-@Output()
-taskTypeList:Array<any>;
-@Output()
-@Input()
-task:Task;
-currentTask:Task=new Task();
-selectType:string;
-type:Task;
-iuserid:number;
+  @Input()
+  @Output()
+  taskTypeList: Array<any>;
+  @Output()
+  @Input()
+  task: Task;
+  currentTask: Task = new Task();
+  selectType: string;
+  type: Task;
+  iuserid: number;
 
 
-  constructor(private appProxy: AppProxy ,private sysTableService:SysTableService , private globalService:GlobalService) { }
+  constructor(private appProxy: AppProxy, private sysTableService: SysTableService, private globalService: GlobalService) { }
 
   minutes: string;
   hours: string;
@@ -33,72 +33,60 @@ iuserid:number;
     //   data => {
     //     //alert("good");
     //     this.taskList = data;
-        this.sysTableService.getValues(SysTableService.dataTables.Task.iSysTableId).then(data => {
-          this.taskTypeList=data;
-          //this.task.iTaskType=this.type['iSysTableRowId'];
-
-          
-          // this.sysTableRowList =  data;
-        //   this.meetingList.forEach(m => {
-        //     m['nvDate'] = m.dtMeetingDate.toLocaleDateString();
-        //     m['nvHour'] = m.dtMeetingDate.toLocaleTimeString();
-        //     m['edit'] ='<div class="edit"></div>';
-        //     m['nvMeetingType'] = this.sysTableRowList.filter(s=> s.iSysTableRowId == m.iMeetingType)[0].nvValue;
-        // });
-        debugger;
+    this.sysTableService.getValues(SysTableService.dataTables.Task.iSysTableId).then(data => {
+      this.taskTypeList = data;
+      //this.task.iTaskType=this.type['iSysTableRowId'];
 
 
-        this.currentTask = Object.assign({},this.task);
-         this.currentTask['dtDate'] = new Date((this.currentTask.dtTaskdatetime).getTime());
-     
-         // this.meeting['dtHour'] = new Date((this.meeting.dtMeetingDate).getHours()) + ':'+new Date((this.meeting.dtMeetingDate).getMinutes());
-         if ((this.task.dtTaskdatetime).getMinutes() < 10)
-           this.minutes = '0' + (this.currentTask.dtTaskdatetime).getMinutes().toString();
-         else
-           this.minutes = (this.currentTask.dtTaskdatetime).getMinutes().toString();
-     
-         if ((this.currentTask.dtTaskdatetime).getHours() < 10)
-           this.hours = '0' + (this.currentTask.dtTaskdatetime).getHours().toString();
-         else
-           this.hours = (this.currentTask.dtTaskdatetime).getHours().toString();
-     
-     
-         this.currentTask['dtHour'] = this.hours + ':' + this.minutes;
-        // }
-        });
-     this.task=new Task();
-     this.task.nvComment=" עם התלמיד: "
-         
-      }
-    // );
-
-    saveTask() {
+      // this.sysTableRowList =  data;
+      //   this.meetingList.forEach(m => {
+      //     m['nvDate'] = m.dtMeetingDate.toLocaleDateString();
+      //     m['nvHour'] = m.dtMeetingDate.toLocaleTimeString();
+      //     m['edit'] ='<div class="edit"></div>';
+      //     m['nvMeetingType'] = this.sysTableRowList.filter(s=> s.iSysTableRowId == m.iMeetingType)[0].nvValue;
+      // });
       debugger;
-      // alert("id:"+ this.task.iTaskId);
-      // alert("dtTaskdate:" +this.task.dtTaskdate);
-      // alert("iTaskType:" +this.task.iTaskType);
-      // alert("tTaskTime:" +this.task.tTaskTime);
-      this.task.dtTaskdatetime = new Date(this.currentTask['dtDate']+' '+this.currentTask['dtHour']);
-      //this.task.iPersonId=
-     // if(this.currentTask.iTaskId == null)
-      //this.task.iPersonId = 1;
 
-      
-      debugger;
-      alert(this.task);
-      alert(this.currentTask);
-      // JSON.parse(localStorage.getItem("user")).iPersonId
-    this.iuserid=this.globalService.getUser()['iUserId'];
-      debugger
-         this.appProxy.post('SetTask', { task:this.task,iUserId:this.iuserid }).then(data => {
-        if (data) {
-          alert("המשימה נוספה בהצלחה!");
-        }
-        else
-          alert("error!");
-      })
-    }
+
+      this.currentTask = Object.assign({}, this.task);
+      this.currentTask['dtDate'] = new Date((this.currentTask.dtTaskdatetime).getTime());
+
+      // this.meeting['dtHour'] = new Date((this.meeting.dtMeetingDate).getHours()) + ':'+new Date((this.meeting.dtMeetingDate).getMinutes());
+      if ((this.task.dtTaskdatetime).getMinutes() < 10)
+        this.minutes = '0' + (this.currentTask.dtTaskdatetime).getMinutes().toString();
+      else
+        this.minutes = (this.currentTask.dtTaskdatetime).getMinutes().toString();
+
+      if ((this.currentTask.dtTaskdatetime).getHours() < 10)
+        this.hours = '0' + (this.currentTask.dtTaskdatetime).getHours().toString();
+      else
+        this.hours = (this.currentTask.dtTaskdatetime).getHours().toString();
+
+
+      this.currentTask['dtHour'] = this.hours + ':' + this.minutes;
+      // }
+    });
+    this.task = new Task();
+    this.task.nvComment = " עם התלמיד: "
 
   }
+  // );
+
+  saveTask() {
+
+    this.task.dtTaskdatetime = new Date(this.currentTask['dtDate'] + ' ' + this.currentTask['dtHour']);
+//    if (this.currentTask.iTaskId == 0)
+      this.task.iPersonId = 1;
+    debugger
+    this.appProxy.post('SetTask', { task: this.task, iUserId: this.globalService.getUser()['iUserId'] }).then(data => {
+      if (data) {
+        alert("המשימה נוספה בהצלחה!");
+      }
+      else
+        alert("error!");
+    })
+  }
+
+}
 
 
