e15d8e2029d0f7caf5f1078792567de9be95da7d
leaZon <40159502+leaZon@users.noreply.github.com>
2018-09-04 15:55:23 +0300

    update cards union

--- Client/src/app/components/cards-union/cards-union.component.ts
+++ Client/src/app/components/cards-union/cards-union.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { AppProxy } from '../../services/app.proxy';
 import { Student } from '../../classes/student';
 import { ActivatedRoute } from '@angular/router';
@@ -21,17 +21,20 @@ model:number;
   students:boolean=false;
 
   id:number;
+  @Output() onClose: EventEmitter<any> = new EventEmitter();
 
   constructor(private activatedRoute: ActivatedRoute, private appProxy:AppProxy,private globalService:GlobalService) { }
 
   ngOnInit() {
+
+
+    
     this.student.dtAddStudentDate=null;
     this.student.dtBirthdate=null;
-// this.student.dtAddStudentDate.toLocaleDateString();
    this.activatedRoute.parent.params.subscribe(params => {
       this.id = params['iPersonId'];
     });
-    this.appProxy.post('GetAvrechStudents', { iPersonId:this.globalService.getUser()['iUserId'] }).then(
+    this.appProxy.post('GetStudentList', { iUserId:this.globalService.getUser()['iUserId'] }).then(
       data => 
       {
         this.studentList = data;
@@ -50,12 +53,12 @@ debugger;
     this.studentList.forEach(e=>{
     if(e.iPersonId==event.currentTarget.value)
       this.student2=e;
+      debugger;
+      
     });
 
    }
-public studentValues;
-public student1Values;
-public  aa;
+
    unionOk()
    {
 
@@ -64,6 +67,16 @@ for(var f in this.student)
  if(this.student[f]==null)
  this.student[f]=this.student1[f];
 }
+if(this.student.nvMotherDeathDate=="")
+this.student.bDeathMother=false;
+else
+this.student.bDeathMother=true;
+
+if(this.student.nvFatherDeathDate=="")
+this.student.bDeathFather=false;
+else
+this.student.bDeathFather=true;
+
 
  this.appProxy.post('UnionCards', { student:this.student,iStudent2:this.student2.iPersonId }).then(
       data => 
@@ -79,5 +92,9 @@ if(this.student1[field]==this.student2[field])
 return true;
 else
 return false;
+}
+close()
+{
+  this.onClose.emit();
 }
 }
