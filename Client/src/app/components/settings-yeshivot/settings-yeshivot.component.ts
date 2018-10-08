9084877d6bafca0bee6e42a2bbbe7ee3bd99d5be
dvoraack <dvoaack@gamil.com>
2018-10-08 17:09:47 +0300

    yeshivot

--- Client/src/app/components/settings-yeshivot/settings-yeshivot.component.ts
+++ Client/src/app/components/settings-yeshivot/settings-yeshivot.component.ts
@@ -60,9 +60,9 @@ export class SettingsYeshivotComponent implements OnInit {
   }
 
   public setYeshiva(yeshiva){
-    // if()
+   //if(yeshiva.culomnClickName=="edit")
        this.editYeshiva(yeshiva);
-    // else 
+  // else 
        this.deleteYeshiva(yeshiva);
   }
 
