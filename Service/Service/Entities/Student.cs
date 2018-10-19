308050787c6a6ec68dd66ea8bfbd46b2963641bf
yehuditm <nod8259@gmail.com>
2018-10-19 12:13:19 +0300

    Update Service/Service/Entities/Student.cs

--- Service/Service/Entities/Student.cs
+++ Service/Service/Entities/Student.cs
@@ -169,7 +169,7 @@ namespace Service.Entities
             try
             {
                 if (base64Image != "")
-                {
+                {                                     
                     student.nvImgStudent = Fileshandler.SaveFileByBase64(base64Image, student.nvImgStudent);
                     DataRow dr = SqlDataAccess.ExecuteDatasetSP("TStudentGetStudentbyId_SLCT", new SqlParameter("iPersonId", student.iPersonId)).Tables[0].Rows[0];
                     string prevName = ObjectGenerator<Student>.GeneratFromDataRow(dr).nvImgStudent;
