345d66e319d5ac828174412f0c578d0896d39572
UdyM <40159584+udymwebit@users.noreply.github.com>
2018-10-19 12:33:37 +0300

    גג

--- Client/src/app/components/student-events/student-events.component.ts
+++ Client/src/app/components/student-events/student-events.component.ts
@@ -80,9 +80,11 @@ export class StudentEventsComponent implements OnInit {
     this.lstDataRows.splice(this.lstDataRows.indexOf(e), 1);
     this.vyTableComponent.refreshTable(this.lstDataRows);
   }
+  from: number = 0;
   eventFlag: boolean = false;
-  newEvent() {
+  newEvent(from) {
     this.eventFlag = true;
+    this.from = from;
   }
   close(){
     this.eventFlag = false;
