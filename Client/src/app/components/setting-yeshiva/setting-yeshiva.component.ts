127c01aa05189f073dc709639eba380109e05fea
shoshi <shoshi3889@gmail.com>
2018-11-05 16:23:51 +0200

    Update Client/src/app/components/setting-yeshiva/setting-yeshiva.component.ts

--- Client/src/app/components/setting-yeshiva/setting-yeshiva.component.ts
+++ Client/src/app/components/setting-yeshiva/setting-yeshiva.component.ts
@@ -86,7 +86,7 @@ export class SettingYeshivaComponent implements OnInit {
             else {
               alert("save!");
               this.closeYeshiva.emit(null);
-              this.addYeshiva.emit(this.yeshiva);
+              //this.addYeshiva.emit(this.yeshiva);
             }
           }
         )
